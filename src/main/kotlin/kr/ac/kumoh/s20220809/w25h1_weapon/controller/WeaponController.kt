e029fe9daa8d796956b256993c67e3b04fc2d46c
damian21012 <mlgk3441@naver.com>
2025-11-29 01:16:06 +0900

    🩺CORS 에러 해결

--- src/main/kotlin/kr/ac/kumoh/s20220809/w25h1_weapon/controller/WeaponController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220809/w25h1_weapon/controller/WeaponController.kt
@@ -2,6 +2,8 @@ package kr.ac.kumoh.s20220809.w25h1_weapon.controller
 
 import kr.ac.kumoh.s20220809.w25h1_weapon.model.Weapon
 import kr.ac.kumoh.s20220809.w25h1_weapon.service.WeaponService
+
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/weapons")
+@CrossOrigin(origins = ["http://localhost:5173"])
 class WeaponController(
     private val service: WeaponService
 ) {
